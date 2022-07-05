d3a517a027cc2c017035b3538e93af05212134a4
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-07-05 10:07:07 +0200

    Ajout d'un contact en BDO

--- contact.php
+++ contact.php
@@ -1,6 +1,19 @@
 <?php
-    include 'function.php';
-    myPrint_r($_GET);
+    include 'config.php';
+    myPrint_r($_POST);
+    // myPrint_r($_SERVER);
+    if ( isset($_POST['action']) AND $_POST['action'] == 'contact') : 
+    
+    $sql = sprintf("INSERT INTO contact SET name='%s', email ='%s', message ='%s'", 
+            ($_POST['nom'] != '') ? $_POST['nom'] : 'inconnu',
+            $_POST['email'],
+            //PHP 8 Plus besoins de commande anti injection SQL
+            strip_tags($_POST['message'])
+);
+    // echo $sql;
+    $connect->query($sql);
+    echo $connect->error;
+    endif;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,11 +26,14 @@
 <body>
     <h1>Contact</h1>
     <!--mettre name par defaut pour ajouter les param a l'url lors de l'envoi'-->
-    <form action="">
+    <form action="" method="post">
         <input type="text" name="nom" placeholder="votre nom"><br>
         <input type="email" name="email" placeholder="Votre email"><br>
         <textarea cols="30" name="message" rows="10"></textarea><br>
+        <input type="hidden" name="action" value="contact">
         <button>Envoyer</button>
     </form>
 </body>
 </html>
+
+<!--Formulaire de recherche c'est en GET les formulaires pour envoi de données en POST-->
